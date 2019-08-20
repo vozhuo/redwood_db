7cd5225fa7b21d4bafdaa818e798ec82f7e7e387
vozhuo <qsx753698@outlook.com>
2019-08-20 20:39:11 +0800

    mirror changes

--- redwood/views.py
+++ redwood/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 import json
 
+
 def index(request):
     return render(request, 'index.html')
 
@@ -16,7 +17,8 @@ def data(request):
 
 
 def image(request):
-    redwood_id = json.loads(request.body.decode()).get('id')
+    # redwood_id = json.loads(request.body.decode()).get('id') for POST
+    redwood_id = request.GET.get('id')
     images = list(Images.objects.filter(redwood_id=redwood_id).values())
     print(images)
     return JsonResponse(images, safe=False)
